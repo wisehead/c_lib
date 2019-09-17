3ff5894dcace81e9fd63f01d87f7a46c6f23b129
wisehead <alex.chenhui@gmail.com>
2019-09-17 14:06:59 +0800

    add2stdio work

--- 15.ipc1/add2stdio.c
+++ 15.ipc1/add2stdio.c
@@ -5,6 +5,10 @@ main(void)
 {
 	int		int1, int2;
 	char	line[MAXLINE];
+	if (setvbuf(stdin, NULL, _IOLBF, 0) != 0)
+		err_sys("setvbuf error");
+	if (setvbuf(stdout, NULL, _IOLBF, 0) != 0)
+		err_sys("setvbuf error");
 
 	while (fgets(line, MAXLINE, stdin) != NULL) {
 		if (sscanf(line, "%d%d", &int1, &int2) == 2) {
