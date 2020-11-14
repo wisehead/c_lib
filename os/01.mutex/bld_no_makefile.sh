88e6848559a7186c2952c4a08c7e7d4307bc8238
wisehead <alex.chenhui@gmail.com>
2020-11-14 15:36:23 +0800

    bld_no_makefile.sh

---
+++ os/01.mutex/bld_no_makefile.sh
@@ -0,0 +1,8 @@
+#############################################################
+#   File Name: bld.sh
+#     Autohor: Hui Chen (c) 2020
+#        Mail: chenhui13@baidu.com
+# Create Time: 2020/11/14-15:36:00
+#############################################################
+#!/bin/sh 
+g++ mutex1.c -lpthread -fpermissive -c
