8326bb52ca034c92a25bd0a1a07a64d3fff4e2c6
wisehead <alex.chenhui@gmail.com>
2022-08-29 14:45:58 +0800

    dlbox

---
+++ os_design_nju/17.virtual_link_load/dlbox/run.sh
@@ -0,0 +1,11 @@
+#############################################################
+#   File Name: run.sh
+#     Autohor: Hui Chen (c) 2022
+#        Mail: chenhui13@xxx.com
+# Create Time: 2022/08/29-14:43:42
+#############################################################
+#!/bin/sh 
+gcc -g dlbox.c -o dlbox
+./dlbox gcc libc.S
+./dlbox gcc libhello.S 
+./dlbox gcc main.S
