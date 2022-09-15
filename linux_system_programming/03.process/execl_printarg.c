38995b5428c78abaf6ba5dd66822e6d3d7008ca6
wisehead <alex.chenhui@gmail.com>
2022-09-15 11:18:28 +0800

    execlp fireforx

--- linux_system_programming/03.process/execl_printarg.c
+++ linux_system_programming/03.process/execl_printarg.c
@@ -10,7 +10,8 @@ int main (void)
   printf("hello\n");
 
   //execl("/bin/ls","ls","-l",NULL);
-  execl("/home/yang/Desktop/123/app","./app","a.c","b.c","c.c",NULL);
+  //execl("/home/yang/Desktop/123/app","./app","a.c","b.c","c.c",NULL);
+  execl("/home/chenhui/github/c_lib/linux_system_programming/03.process/printarg","./printarg","a.c","b.c","c.c",NULL);
 
   //it's finish!!!!
   printf("world\n");
