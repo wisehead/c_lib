2c0785c1a9846f871b9d0d128fea5396e435f1fe
wisehead <alex.chenhui@gmail.com>
2022-09-16 12:23:58 +0800

    tid long int

--- linux_system_programming/08.thread/thread1.c
+++ linux_system_programming/08.thread/thread1.c
@@ -9,7 +9,9 @@ void *th_fun(void *arg)
 {
   int *p=(int *)arg;
   printf("thread PID=%d\n",getpid());
-  printf("thread ID=%x\n" ,(int)pthread_self());
+  //printf("thread ID=%x\n" ,(int)pthread_self());
+  printf("thread ID=%ld\n" ,(int)pthread_self());
+
   printf("thread *arg=%d\n",*p);
   sleep(1);
 }
@@ -20,8 +22,10 @@ int main(void)
   int n=10;
 
   pthread_create(&tid,NULL,th_fun,(void*)&n);
-  printf("main thread ID = %x\n",(int)pthread_self());
-  printf("main child thread ID=%x\n",(int)tid);
+  //printf("main thread ID = %x\n",(int)pthread_self());
+  printf("main thread ID = %ld\n",(int)pthread_self());
+  //printf("main child thread ID=%x\n",(int)tid);
+  printf("main child thread ID=%ld\n",(int)tid);
   printf("main PID = %d\n",getpid());
   sleep(2);
 
