988dede2675a935a023558c74238d2d00940b301
wisehead <alex.chenhui@gmail.com>
2022-09-20 10:15:00 +0800

    fork server

--- linux_system_programming/12.high_concurrency_server/multi_process/client.c
+++ linux_system_programming/12.high_concurrency_server/multi_process/client.c
@@ -14,35 +14,39 @@
 
 int main(int argc,char *argv[])
 {
-  int confd,len;
-  char ipstr[]="127.0.0.1";
-  char buf[MAXLINE];
-  struct sockaddr_in serveraddr;
-  /*
-  if (argc<2){
-    printf("./client str\n");
-    exit(1);
-  }
-  */
-  //1.create a socket
-  confd=socket(AF_INET,SOCK_STREAM,0);
+	int confd,len;
+	char ipstr[]="127.0.0.1";
+	char buf[MAXLINE];
+	struct sockaddr_in serveraddr;
+	/*
+	   if (argc<2){
+	   printf("./client str\n");
+	   exit(1);
+	   }
+	   */
+	//1.create a socket
+	confd=socket(AF_INET,SOCK_STREAM,0);
 
-  //2.initializes the server address
-  bzero(&serveraddr,sizeof(serveraddr));
-  serveraddr.sin_family=AF_INET;
-  //ipadress
-  inet_pton(AF_INET,ipstr,&serveraddr.sin_addr.s_addr);
-  serveraddr.sin_port=htons(SERVER_PORT);
-  //3.connect
-  connect(confd,(struct sockaddr *)&serveraddr,sizeof(serveraddr));
-  while(fgets(buf,sizeof(buf),stdin)){
-  //4.request the server to process the data
-  write(confd,buf,sizeof(buf));
-  len=read(confd,buf,sizeof(buf));
-  write(STDOUT_FILENO,buf,len);
-  }
-  //5.close socket
-  close(confd);
-  return 0;
+	//2.initializes the server address
+	bzero(&serveraddr,sizeof(serveraddr));
+	serveraddr.sin_family=AF_INET;
+	//ipadress
+	inet_pton(AF_INET,ipstr,&serveraddr.sin_addr.s_addr);
+	serveraddr.sin_port=htons(SERVER_PORT);
+	//3.connect
+	connect(confd,(struct sockaddr *)&serveraddr,sizeof(serveraddr));
+	while(fgets(buf,sizeof(buf),stdin)){
+		//4.request the server to process the data
+		int xlen = strlen(buf);
+		printf("buf:%s, len:%d, strlen:%d\n", buf, len, xlen);
+		//write(confd,buf,sizeof(buf));
+		write(confd,buf,xlen+1);
+		len=read(confd,buf,sizeof(buf));
+		printf("buf:%s, len:%d\n", buf, len);
+		write(STDOUT_FILENO,buf,len);
+	}
+	//5.close socket
+	close(confd);
+	return 0;
 
 }
