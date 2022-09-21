2d8c5d85481c58e0028b937bd6d144eadc68b4ed
wisehead <alex.chenhui@gmail.com>
2022-09-21 11:55:23 +0800

    group multicast, 0.0.0.0 works, not 127.0.0.1

--- linux_system_programming/12.high_concurrency_server/group_broadcast/server.c
+++ linux_system_programming/12.high_concurrency_server/group_broadcast/server.c
@@ -10,8 +10,14 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <net/if.h>
+
 #define SERVER_PORT 8000
+#define CLIENT_PORT 9000
+
 #define MAXLINE 4096
+
+#define GROUP "239.0.0.2"
 
 int main (void)
 {
@@ -21,6 +27,7 @@ int main (void)
 	char ipstr[INET_ADDRSTRLEN];
 	socklen_t clientlen;
 	ssize_t len;
+	struct ip_mreqn group;
 	//create a socket
 	sockfd=socket(AF_INET,SOCK_DGRAM,0);
 
@@ -31,20 +38,30 @@ int main (void)
 	//bind
 	bind(sockfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
 
-	while(1){
-		clientlen=sizeof(clientaddr);
-		len=recvfrom(sockfd,buf,sizeof(buf),0,(struct sockaddr*)&clientaddr,
-				&clientlen);
-		printf("client IP %s\tPORT%d\n",
-				inet_ntop(AF_INET,&clientaddr.sin_addr.s_addr,
-					ipstr,sizeof(ipstr)),ntohs(clientaddr.sin_port));
+	/*
+	   int setsockopt(int sockfd, int level, int optname,                
+	   const void *optval, socklen_t optlen);
+	   */
 
-		i=0;
-		while(i<len){
-			buf[i]=toupper(buf[i]);
-			i++;
-		}
-		sendto(sockfd,buf,len,0,(struct sockaddr*)&clientaddr,sizeof(clientaddr));
+	/* group address*/
+	inet_pton(AF_INET, GROUP, &group.imr_multiaddr);
+	/* local address*/
+	inet_pton(AF_INET, "0.0.0.0", &group.imr_address);
+	/*eth0 --> index, command: ip ad*/
+	group.imr_ifindex = if_nametoindex("eth0");
+
+	setsockopt(sockfd, IPPROTO_IP, IP_MULTICAST_IF, &group, sizeof(group));
+
+	bzero(&clientaddr,sizeof(clientaddr));
+	clientaddr.sin_family=AF_INET;  //ipv4
+	/*construct client address ip+port*/
+	inet_pton(AF_INET, GROUP, &clientaddr.sin_addr.s_addr);
+	clientaddr.sin_port = htons(CLIENT_PORT);
+
+	while(1){
+		fgets(buf, sizeof(buf), stdin);
+		printf("buf:%s\n", buf);
+		sendto(sockfd,buf,strlen(buf),0,(struct sockaddr*)&clientaddr,sizeof(clientaddr));
 	}
 
 	close(sockfd);
