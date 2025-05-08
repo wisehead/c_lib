11965339d535b03c401319544e9c77e23dbb649f
Hui Chen <alex.chenhui@gmail.com>
2025-05-08 20:18:42 +0800

    myshell

--- linux_system_programming/04.ipc/mmap_w.c
+++ linux_system_programming/04.ipc/mmap_w.c
@@ -14,34 +14,34 @@
 
 void sys_err(char *str,int exitno)
 {
-  perror(str);
-  exit(exitno);
+	perror(str);
+	exit(exitno);
 }
 
 int main(int argc,char *argv[])
 {
-  char *mm;
-  int fd,i=0;
-  if(argc<2)
-    sys_err("app filename\n",1);
-  fd=open(argv[1],O_RDWR|O_CREAT,0777);
-  if(fd<0)
-    sys_err("open",1);
+	char *mm;
+	int fd,i=0;
+	if(argc<2)
+		sys_err("app filename\n",1);
+	fd=open(argv[1],O_RDWR|O_CREAT,0777);
+	if(fd<0)
+		sys_err("open",1);
 
-  if(lseek(fd,MAPLEN-1,SEEK_SET)<0)
-    sys_err("open",3);
-  if(write(fd,"\0",1)<0)
-    sys_err("write",4);
+	if(lseek(fd,MAPLEN-1,SEEK_SET)<0)
+		sys_err("open",3);
+	if(write(fd,"\0",1)<0)
+		sys_err("write",4);
 
-  mm=mmap(NULL,MAPLEN,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
-  close(fd);
-  if(mm==MAP_FAILED)
-    sys_err("mmap",2);
+	mm=mmap(NULL,MAPLEN,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+	close(fd);
+	if(mm==MAP_FAILED)
+		sys_err("mmap",2);
 
-  while(1){
-    sprintf(mm,"hello%d",i++);
-    sleep(1);
-  }
-  munmap(mm,MAPLEN);
-  return 0;
+	while(1){
+		sprintf(mm,"hello%d",i++);
+		sleep(1);
+	}
+	munmap(mm,MAPLEN);
+	return 0;
 }
